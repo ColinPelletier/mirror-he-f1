0e4364a5791893130e83c9a4c21dcf7fe17f264d
Colin Pelletier <colin.pelletier@he-arc.ch>
2021-03-08 11:24:10 +0100

    add base project

---
+++ he-f1/src/main/java/ch/hearc/hef1/HeF1Application.java
@@ -0,0 +1,13 @@
+package ch.hearc.hef1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeF1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeF1Application.class, args);
+	}
+
+}
