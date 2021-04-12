3ae0761c8a613e4955467fa2373249e3acf9c932
Alan.chambaz <alan.chambaz@he-arc.ch>
2021-04-12 13:29:06 +0200

    Correct login an signup errors
    And auto indent with prettier

--- he-f1/src/main/java/ch/hearc/hef1/controller/TeamController.java
+++ he-f1/src/main/java/ch/hearc/hef1/controller/TeamController.java
@@ -1,12 +1,9 @@
 package ch.hearc.hef1.controller;
 
-import java.text.NumberFormat;
-import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import javax.management.RuntimeErrorException;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
-
+import ch.hearc.hef1.model.Car;
+import ch.hearc.hef1.model.CarPiece;
 import ch.hearc.hef1.model.Team;
 import ch.hearc.hef1.repository.CarRepository;
 import ch.hearc.hef1.repository.TeamRepository;
@@ -169,16 +167,18 @@ public class TeamController {
 	//
 	//
 	// }
-	@PostMapping("/createteam")
-	public String saveTeam(@Validated @ModelAttribute Team team, BindingResult errors, Model model,
-			RedirectAttributes redirAttrs) {
+	// @PostMapping("/createteam")
+	// public String saveTeam(@Validated @ModelAttribute Team team, BindingResult
+	// errors, Model model,
+	// RedirectAttributes redirAttrs) {
 
-		if (!errors.hasErrors()) {
-			if (team.validate())
-			else
-				teamRepository.save(team);
-				throw new RuntimeException("The team is not complete ! Please fill all the fields");
-		}
-	}
-		return ((errors.hasErrors()) ? "team" : "redirect:/");
+	// if (!errors.hasErrors()) {
+	// if (team.validate())
+	// else
+	// teamRepository.save(team);
+	// throw new RuntimeException("The team is not complete ! Please fill all the
+	// fields");
+	// }
+	// }
+	// return ((errors.hasErrors()) ? "team" : "redirect:/");
 }
--- he-f1/src/main/java/ch/hearc/hef1/controller/UserController.java
+++ he-f1/src/main/java/ch/hearc/hef1/controller/UserController.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.management.relation.Role;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,24 +22,15 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	/**
-	 * Select the login view to render by returning its name
-	 */
-	@GetMapping("/login")
-	public String login(Map<String, Object> model) {	
-		model.put("author", new User());
-		return "login";
-	}
-	
 	@GetMapping("/signup")
 	public String signup(Map<String, Object> model) {
 		User user = new User();
 		model.put("user", user);
-		
+
 		List<Enum> roles = new ArrayList<Enum>(EnumSet.allOf(UserRole.class));
-		
+
 		model.put("roles", roles);
-		
+
 		return "signup";
 	}
 
@@ -52,7 +39,7 @@ public class UserController {
 
 		User userExists = userService.findUserByEmail(author.getEmail());
 		model.put("roles", UserRole.values());
-		//System.out.println("name = " + author.getUsername());
+		// System.out.println("name = " + author.getUsername());
 		System.out.println("================================================");
 		System.out.println(author.getRole());
 		System.out.println("================================================");
@@ -60,13 +47,10 @@ public class UserController {
 		if (userExists != null) {
 			return ("redirect:/signup?error=true");
 		}
-		
-		if (bindingResult.hasErrors()) 
-		{
+
+		if (bindingResult.hasErrors()) {
 			return ("redirect:/signup?error=true");
-		} 
-		else 
-		{
+		} else {
 			userService.saveUser(author);
 			model.put("msg", "User has been registered successfully!");
 			model.put("author", new User());
