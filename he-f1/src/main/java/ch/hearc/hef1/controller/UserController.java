e6d25b73284d023270b713a90bc211dae9c96f95
sebastie.roso <sebastien.roso@he-arc.ch>
2021-04-19 09:46:20 +0200

    update unit test

--- he-f1/src/main/java/ch/hearc/hef1/controller/UserController.java
+++ he-f1/src/main/java/ch/hearc/hef1/controller/UserController.java
@@ -30,20 +30,20 @@ public class UserController {
 	 * Select the login view to render by returning its name
 	 */
 	@GetMapping("/login")
-	public String login(Map<String, Object> model) {	
+	public String login(Map<String, Object> model) {
 		model.put("author", new User());
 		return "login";
 	}
-	
+
 	@GetMapping("/signup")
 	public String signup(Map<String, Object> model) {
 		User user = new User();
 		model.put("user", user);
-		
+
 		List<Enum> roles = new ArrayList<Enum>(EnumSet.allOf(UserRole.class));
-		
+
 		model.put("roles", roles);
-		
+
 		return "signup";
 	}
 
@@ -52,7 +52,7 @@ public class UserController {
 
 		User userExists = userService.findUserByEmail(author.getEmail());
 		model.put("roles", UserRole.values());
-		//System.out.println("name = " + author.getUsername());
+		// System.out.println("name = " + author.getUsername());
 		System.out.println("================================================");
 		System.out.println(author.getRole());
 		System.out.println("================================================");
@@ -60,13 +60,10 @@ public class UserController {
 		if (userExists != null) {
 			return ("redirect:/signup?error=true");
 		}
-		
-		if (bindingResult.hasErrors()) 
-		{
+
+		if (bindingResult.hasErrors()) {
 			return ("redirect:/signup?error=true");
-		} 
-		else 
-		{
+		} else {
 			userService.saveUser(author);
 			model.put("msg", "User has been registered successfully!");
 			model.put("author", new User());
