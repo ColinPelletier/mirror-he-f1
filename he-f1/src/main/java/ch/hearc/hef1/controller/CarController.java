28527e74332ba4e0c7a0477d1150c36f7bc91c9c
sebastie.roso <sebastien.roso@he-arc.ch>
2021-04-19 12:32:01 +0200

    show piece id

--- he-f1/src/main/java/ch/hearc/hef1/controller/CarController.java
+++ he-f1/src/main/java/ch/hearc/hef1/controller/CarController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import ch.hearc.hef1.model.Car;
 import ch.hearc.hef1.model.CarPiece;
@@ -33,5 +34,21 @@ public class CarController {
         model.put("carPieces", carPieces);
 
         return "test-display";
+    }
+
+    @PostMapping("/car/update/{strPieceId}")
+    public String updatePiece(@PathVariable String strPieceId, Map<String, Object> model) {
+        long carId = Long.parseLong(strPieceId);
+        System.out.println("piece id = " + carId);
+        // model.put("title", homeTitle);
+
+        // // Put the todo list by the given author
+        // model.put("todos", todo.getAllTodosByAuthor(strAuthor));
+        // model.put("author", strAuthor);
+        // model.put("totalTime", todo.getTotalTimeByAuthor(strAuthor));
+
+        // // Return the page "home.html"
+        // return "todoByAuthor";
+        return ("redirect:/recruiting?recruited=true");
     }
 }
