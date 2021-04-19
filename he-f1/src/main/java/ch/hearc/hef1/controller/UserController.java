e47314646b55bce2cbd84370bceb0bf4127871d1
Alan.chambaz <alan.chambaz@he-arc.ch>
2021-04-19 12:45:37 +0200

    Add recruiting page

--- he-f1/src/main/java/ch/hearc/hef1/controller/UserController.java
+++ he-f1/src/main/java/ch/hearc/hef1/controller/UserController.java
@@ -1,9 +1,12 @@
 package ch.hearc.hef1.controller;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +14,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import ch.hearc.hef1.model.User;
 import ch.hearc.hef1.model.UserRole;
+import ch.hearc.hef1.repository.UserRepository;
 import ch.hearc.hef1.service.UserService;
 
 @Controller
@@ -21,6 +29,9 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	UserRepository userRepository;
 
 	@GetMapping("/signup")
 	public String signup(Map<String, Object> model) {
@@ -35,14 +46,10 @@ public class UserController {
 	}
 
 	@PostMapping("/signup")
-	public String createAuthor(@Valid User author, BindingResult bindingResult, Map<String, Object> model) {
+	public String createUser(@Valid User user, BindingResult bindingResult, Map<String, Object> model) {
 
-		User userExists = userService.findUserByEmail(author.getEmail());
+		User userExists = userService.findUserByEmail(user.getEmail());
 		model.put("roles", UserRole.values());
-		// System.out.println("name = " + author.getUsername());
-		System.out.println("================================================");
-		System.out.println(author.getRole());
-		System.out.println("================================================");
 
 		if (userExists != null) {
 			return ("redirect:/signup?error=true");
@@ -51,11 +58,59 @@ public class UserController {
 		if (bindingResult.hasErrors()) {
 			return ("redirect:/signup?error=true");
 		} else {
-			userService.saveUser(author);
+			userService.saveUser(user);
 			model.put("msg", "User has been registered successfully!");
-			model.put("author", new User());
+			model.put("user", new User());
 
 			return "login";
+		}
+	}
+
+	@GetMapping("/recruiting")
+	public String recruiting(@RequestParam(value = "selectedRole", required = false) String strRole,
+			Map<String, Object> model) {
+
+		if (strRole != null) {
+			UserRole role = UserRole.valueOf(strRole);
+			model.put("users", userRepository.findByRole(role));
+		} else {
+			model.put("users", userRepository.findAll());
+		}
+
+		List<Enum> roles = new ArrayList<Enum>(EnumSet.allOf(UserRole.class));
+		model.put("roles", roles);
+
+		return "recruiting";
+	}
+
+	@PostMapping("/recruit/{strUserId}")
+	public String recruit(@PathVariable String strUserId, Map<String, Object> model) {
+
+		// Check id validity
+		long userId;
+
+		try {
+			userId = Long.parseLong(strUserId);
+		} catch (NumberFormatException e) {
+			System.err.println("id must be an integer");
+			return ("redirect:/recruiting?error=true");
+		}
+
+		Optional<User> user = userRepository.findById(userId);
+
+		if (user.isPresent()) {
+			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			User authenticatedUser = userService.findUserByUsername(auth.getName());
+
+			user.get().setTeam(authenticatedUser.getTeam());
+			userService.saveUser(user.get());
+			model.put("msg", "User has been recruited successfully!");
+
+			return ("redirect:/recruiting?recruited=true");
+
+		} else {
+			return ("redirect:/recruiting?error=true");
+			// return ("redirect:/notfound");
 		}
 	}
 
--- he-f1/src/main/java/ch/hearc/hef1/model/UserRole.java
+++ he-f1/src/main/java/ch/hearc/hef1/model/UserRole.java
@@ -1,17 +1,15 @@
 package ch.hearc.hef1.model;
 
 public enum UserRole {
-	
-	MANAGER("Manageur"),
-    ENGINEER("Ingénieur"),
-    MECHANICIAN("Mécanicien");
-    
+
+    MANAGER("Manager"), ENGINEER("Engineer"), MECHANICIAN("Mechanician");
+
     private String description;
-	
-	UserRole(final String description) {
+
+    UserRole(final String description) {
         this.description = description;
     }
-	
+
     public String getDescription() {
         return this.description;
     }
