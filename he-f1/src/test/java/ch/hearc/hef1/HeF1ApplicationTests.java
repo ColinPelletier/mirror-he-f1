08980ca710e296ccf4a40a4b5f0cbc00587a60f4
colin.pelletie <colin.pelletier@he-arc.ch>
2021-04-25 14:30:42 +0200

    Disable database requirements

--- he-f1/src/test/java/ch/hearc/hef1/HeF1ApplicationTests.java
+++ he-f1/src/test/java/ch/hearc/hef1/HeF1ApplicationTests.java
@@ -1,17 +1,15 @@
 package ch.hearc.hef1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class HeF1ApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void simpleTest() {
 		new TestAssertTrue().testAssertTrue();
 	}
 
