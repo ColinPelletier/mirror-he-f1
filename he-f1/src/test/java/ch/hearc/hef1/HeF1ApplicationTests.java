75ad2c6cae6fc29f3ae2332e4cdef429224b54be
colin.pelletie <colin.pelletier@he-arc.ch>
2021-04-26 11:35:06 +0200

    Add test.properties file to not use DB with tests

--- he-f1/src/test/java/ch/hearc/hef1/HeF1ApplicationTests.java
+++ he-f1/src/test/java/ch/hearc/hef1/HeF1ApplicationTests.java
@@ -3,9 +3,11 @@ package ch.hearc.hef1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
+@TestPropertySource("classpath:test.properties")
 class HeF1ApplicationTests {
 
 	@Test
