c48d0cdd3d40994add167ea3ce467b039f38fc34
colin.pelletie <colin.pelletier@he-arc.ch>
2021-04-26 09:21:09 +0200

    Add populate and ci/cd jobs

---
+++ he-f1/src/main/resources/data.sql
@@ -0,0 +1,47 @@
+
+/* Points List */
+insert into points (id, money, nb_points, position) VALUES (1, 25000, 25, 1);
+insert into points (id, money, nb_points, position) VALUES (2, 18000, 18, 2);
+insert into points (id, money, nb_points, position) VALUES (3, 15000, 15, 3);
+insert into points (id, money, nb_points, position) VALUES (4, 12000, 12, 4);
+insert into points (id, money, nb_points, position) VALUES (5, 10000, 10, 5);
+insert into points (id, money, nb_points, position) VALUES (6, 8000, 8, 6);
+insert into points (id, money, nb_points, position) VALUES (7, 6000, 6, 7);
+insert into points (id, money, nb_points, position) VALUES (8, 4000, 4, 8);
+insert into points (id, money, nb_points, position) VALUES (9, 2000, 2, 9);
+insert into points (id, money, nb_points, position) VALUES (10, 0, 1, 10);
+insert into points (id, money, nb_points, position) VALUES (11, 0, 0, 11);
+insert into points (id, money, nb_points, position) VALUES (12, 0, 0, 12);
+insert into points (id, money, nb_points, position) VALUES (13, 0, 0, 13);
+insert into points (id, money, nb_points, position) VALUES (14, 0, 0, 14);
+insert into points (id, money, nb_points, position) VALUES (15, 0, 0, 15);
+insert into points (id, money, nb_points, position) VALUES (16, 0, 0, 16);
+insert into points (id, money, nb_points, position) VALUES (17, 0, 0, 17);
+insert into points (id, money, nb_points, position) VALUES (18, 0, 0, 18);
+insert into points (id, money, nb_points, position) VALUES (19, 0, 0, 19);
+insert into points (id, money, nb_points, position) VALUES (20, 0, 0, 20);
+
+/* Grand-Prix */
+insert into grand_prix (id, name, date, country) VALUES (1, "Bahrain Grand Prix (Bahrain International Circuit)", "2021-03-28", "Bahrain");
+insert into grand_prix (id, name, date, country) VALUES (2, "Emilia Romagna Grand Prix (Autodromo Enzo e Dino Ferrari)", "2021-04-18", "Italy");
+insert into grand_prix (id, name, date, country) VALUES (3, "Portuguese Grand Prix (Autodromo Internacional do Algarve)", "2021-05-02", "Portugal");
+insert into grand_prix (id, name, date, country) VALUES (4, "Spanish Grand Prix (Circuit de Barcelona-Catalunya)", "2021-05-09", "Spain");
+insert into grand_prix (id, name, date, country) VALUES (5, "Monaco Grand Prix (Circuit de Monaco in Monte Carlo)", "2021-05-23", "Monaco");
+insert into grand_prix (id, name, date, country) VALUES (6, "Azerbaijan Grand Prix (Baku City Circuit)", "2021-06-06", "Azerbaijan");
+insert into grand_prix (id, name, date, country) VALUES (7, "Canadian Grand Prix (Circuit Gilles-Villeneuve)", "2021-06-13", "Canada");
+insert into grand_prix (id, name, date, country) VALUES (8, "French Grand Prix (Circuit Paul Ricard)", "2021-06-27", "France");
+insert into grand_prix (id, name, date, country) VALUES (9, "Austrian Grand Prix (Spielberg)", "2021-07-04", "Austria");
+insert into grand_prix (id, name, date, country) VALUES (10, "British Grand Prix (Silverstone)", "2021-07-18", "Great Britain");
+insert into grand_prix (id, name, date, country) VALUES (11, "Hungarian Grand Prix (Hungaroring)", "2021-08-01", "Hungary");
+insert into grand_prix (id, name, date, country) VALUES (12, "Belgian Grand Prix (Spa-Francorchamps)", "2021-08-29", "Belgium");
+insert into grand_prix (id, name, date, country) VALUES (13, "Dutch Grand Prix (Zandvoort)", "2021-09-05", "Netherlands");
+insert into grand_prix (id, name, date, country) VALUES (14, "Italian Grand Prix (Monza)", "2021-09-12", "Italy");
+insert into grand_prix (id, name, date, country) VALUES (15, "Russian Grand Prix (Sochi Autodrom)", "2021-09-26", "Russia");
+insert into grand_prix (id, name, date, country) VALUES (16, "Singapore Grand Prix (Marina Bay)", "2021-10-03", "Singapore");
+insert into grand_prix (id, name, date, country) VALUES (17, "Japanese Grand Prix (Suzuka International Racing Course)", "2021-10-10", "Japan");
+insert into grand_prix (id, name, date, country) VALUES (18, "United States Grand Prix (Circuit of The Americas)", "2021-10-24", "United States");
+insert into grand_prix (id, name, date, country) VALUES (19, "Mexico City Grand Prix (Autodromo Hermanos Rodriguez)", "2021-10-31", "Mexico");
+insert into grand_prix (id, name, date, country) VALUES (20, "Sao Paulo Grand Prix (Autodromo Jose Carlos Pace)", "2021-11-07", "Brazil");
+insert into grand_prix (id, name, date, country) VALUES (21, "Australian Grand Prix (Melbourne)", "2021-11-21", "Australia");
+insert into grand_prix (id, name, date, country) VALUES (22, "Saudi Arabia Grand Prix (Jeddah Street Circuit)", "2021-12-05", "Saudi Arabia");
+insert into grand_prix (id, name, date, country) VALUES (23, "Abu Dhabi Grand Prix (Yas Marina)", "2021-12-12", "Abu Dhabi");
